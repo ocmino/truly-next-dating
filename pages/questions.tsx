79194d4282609d90aec2ca6545672d835f28793f
Jonathan Lindqvist <ocmino@gmail.com>
2022-05-09 12:43:22 +0200

    add questions JSON file and questions tsx file

---
+++ pages/questions.tsx
@@ -0,0 +1,62 @@
+import Head from "next/head";
+import { useState } from "react";
+import questions from "../questions.json";
+
+
+
+export default function Questionaire() {
+  
+  
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const handlePrevious = () => {
+    const prevQues = currentQuestion - 1;
+    prevQues >= 0 && setCurrentQuestion(prevQues);
+  };
+  
+  const handleNext = () => {
+    const nextQues = currentQuestion + 1;
+    nextQues < questions.length && setCurrentQuestion(nextQues);
+  };
+  
+  
+  return (
+
+    <div className="flex flex-col w-screen px-5 h-screen bg-[#1A1A1A] justify-center items-center">
+
+      <div className="flex flex-col items-start w-full">
+        <h4 className="mt-10 text-xl text-white/60">
+          Fråga {currentQuestion + 1} av {questions.length}
+        </h4>
+        <div className="mt-4 text-2xl text-white">
+          {questions[currentQuestion].question}
+        </div>
+      </div>
+
+      <div className="flex flex-col w-full">
+        {questions[currentQuestion].answerOptions.map
+      ((answer, index) => (
+          <div
+            key={index}
+            className="flex items-center w-full py-4 pl-5 m-2 ml-0 space-x-2 border-2 cursor-pointer bg-white/5 border-white/10 rounded-xl">
+            <input type="radio" className="w-6 h-6 bg-black" />
+            <p className="ml-6 text-white">{answer.answer}</p>
+          </div>
+        ))}
+      </div>
+
+      <div className="flex justify-between w-full mt-4 text-white">
+        <button
+          onClick={handlePrevious}
+          className="w-[49%] py-3 bg-indigo-600 rounded-lg">
+          Föregående
+        </button>
+        <button
+          onClick={handleNext}
+          className="w-[49%] py-3 bg-indigo-600 rounded-lg">
+          Fortsätt
+        </button>
+      </div>
+
+    </div>
+  )
+}
