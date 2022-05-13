8dae84320bdfe528770099635649108c3bc121ef
HelenaOstlund <92034375+HelenaOstlund@users.noreply.github.com>
2022-05-13 17:57:07 +0200

    new relationtypePage

--- pages/relationaltype.jsx
+++ pages/relationaltype.jsx
@@ -2,6 +2,7 @@
 import questions from "../questions.json";
 import Questionaire from "./questions";
 import { useState } from "react";
+import Link from "next/link";
 
 
 
@@ -11,6 +12,36 @@ export default function RelationalType() {
     const [score] = useState(0);
    
     return (
-        <p>You scored {score} out of {questions.length}</p>
+        <div className="relationaltypePage"> 
+            <div className="relationalText">
+                <h1 className="relationalfirstText">Din relationstyp är</h1>
+               <div> <h1 className="undvikande">UNDVIKANDE</h1></div>
+
+
+
+            </div>  
+            <div className="scoreReveal">
+                <p>Ditt värde ( {score} av {questions.length})</p>
+            </div>
+            <div className="informationText">   
+                <p>Ett värde över 3,11 resulterar i en undvikande anknytning. Ett högre värde kan tyda på en otryggare anknytning</p>
+            </div>
+            <div className="descriptiveText">
+                <p> 
+                Mer tankestyrd än känslostyrd.
+                Ofta social & utåtriktad.
+                Upplevs som trygg & självständig.
+                Känns svårt att tala om känslor.
+                Hittar snabbt ”brister” hos sin partner.
+
+                </p>
+            </div>
+
+            <p><button><p>
+          <Link href="">
+            <a>Fortsätt</a>
+          </Link>
+        </p></button></p>
+        </div>
     )
 }
