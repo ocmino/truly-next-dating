a1208fe862612ba73694b9cb5f31f5766d285c87
HelenaOstlund <92034375+HelenaOstlund@users.noreply.github.com>
2022-05-17 16:11:45 +0200

    added serverside props, get firestore, projektId, client email, private key

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,19 @@
 import Link from "next/link";
 import Image from 'next/image'
 import logo from '../public/static/truly-logo.png'
+import { firestore } from "../utils/firebase";
 
-export default function AboutPage() {
+export default function AboutPage({ users }) {
+  console.log(users)
   return (
+  
     <div className="page about">
       <div>
       {/* logo */}
       <h1 className="trulyLogin"><Image src={logo} width={50} height={50}/> truly</h1>
-      </div>
       
+      </div>
+     
       <div>
       <button className="indexButtonLogin"><p>
           <Link href="/loginpage">
@@ -30,4 +34,24 @@ export default function AboutPage() {
       
     </div>
   );
+}
+// serverside code
+export async function getServerSideProps(){
+
+const snapshots = await firestore.collection('users').get()
+const users = snapshots.docs.map(doc => {
+  return  {
+    id: doc.id,
+    ...doc.data()
+  }
+})
+
+console.log(users)
+  return {
+    props: {
+      users : users
+    }
+  }
+
+
 }
