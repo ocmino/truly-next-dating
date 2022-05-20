d87975fc32910441ebeee65655d2da1171adb206
Jonathan Lindqvist <ocmino@gmail.com>
2022-05-20 11:10:17 +0200

    move firebase users to another file

--- pages/users.tsx
+++ pages/users.tsx
@@ -4,7 +4,7 @@ import logo from '../public/static/truly-logo.png'
 import React, { useState } from "react";
 import { firestore } from "../utils/firebase";
 
-export default function SignUp({ users }) {
+export default function Users({ users }) {
 console.log(users)
 
 const Login = () => {
@@ -25,28 +25,9 @@ const Login = () => {
 
 
     <div className="page about">
-      <div>
-      {/* logo */}
-    {  <h1 className="trulyLogin"><Image src={logo} width={50} height={50}/> truly</h1>}
-      </div>
-      
-    {  <div>
-        <p><input type="email" className="input-email" placeholder="E-post"></input></p>
-        <p><input type="password" className="input-password" placeholder="Lösenord"></input></p>
-        <p><button className="loginButton"><p>
-          <Link href="">
-            <a className="">Skapa konto</a>
-          </Link>
-        </p></button></p>
-        
-        <p>
-          <Link href="/contactform">
-            <a className="problemlogin">Problem att logga in?</a>
-          </Link>
-        </p> 
-      </div>}
+  
 
-      {/* <ul>
+      <ul>
         {users.map((user) => {
           return (
             <li key={user.id}>
@@ -56,7 +37,7 @@ const Login = () => {
             </li>
           )
         })}
-      </ul> */}
+      </ul>
 
       
     </div>
