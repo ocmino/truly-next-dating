1fb96ab93fadda02aa71e0c706c9808e8fdf0efc
HelenaOstlund <92034375+HelenaOstlund@users.noreply.github.com>
2022-05-21 23:03:50 +0200

    added content to kannetecken page

--- pages/femkannetecken.js
+++ pages/femkannetecken.js
@@ -5,6 +5,15 @@ export default function Femkannetecken(){
             <div className="headline">
                 <h2>Fem kännetecken</h2>
             </div>
+            <div className="fivesignscontent">
+                <p>
+                    Mer tankestyrd än känslostyrd.<br></br>
+                    Ofta social och utåtriktad.<br></br>
+                    Upplevs som trygg & självständig.<br></br>
+                    Känns svårt att tala om känslor.<br></br>
+                    Hittar snabbt "brister" hos sin partner.
+                </p>
+            </div>
         </div>
     )
 }
