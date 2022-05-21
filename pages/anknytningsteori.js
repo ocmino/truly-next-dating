683197ab1a274cea303756c1dc72fdbcbc17a2c8
HelenaOstlund <92034375+HelenaOstlund@users.noreply.github.com>
2022-05-21 22:42:43 +0200

    new styles for anknytningsteori page

--- pages/anknytningsteori.js
+++ pages/anknytningsteori.js
@@ -2,19 +2,19 @@ export default function Anknytningsteori() {
 
     return (
         <div className="Infopages">
-            
+
             <div className="headline">
                 <h2>Anknytningsteori</h2>
             </div>
             <div className="headline2">
-                <h5>Alla har ett dejtingmönster</h5>
+                <h6><b>Alla har ett dejtingmönster</b></h6>
             </div>
             <div className="infocontent">
                 <p> Väldigt många tror att det är något fel på dem som gör att de inte får sitt kärleksliv att fungera. De tror att de är konstiga, annorlunda och ensamma om att ha problem. Men nej, absolut inte. Det är inte en slump att vissa dejtar med stor lätthet och framgång medan andra sliter och kämpar utan att komma i närheten av samma enkelhet. Vad beror det på då? Vilken anknytning som du fått från dina föräldrar kan ge dig en del svar.</p>
             </div>
 
             <div className="headline2">
-                <h5>Vad är anknytning?</h5>
+                <h6><b>Vad är anknytning?</b></h6>
             </div>
             <div className="infocontent">
                 <p>En del människor har lätt att leva i nära relationer, andra har det inte. Vilken anknytning du har är en stor del av förklaringen. Hur du lärde dig att relatera till din närmaste omgivning från födseln har en tendens att bli det mönster du följer resten av livet. 
@@ -22,9 +22,9 @@ export default function Anknytningsteori() {
                     Din anknytning utvecklas särskilt under de första två levnadsåren, men befästs eller förändras av erfarenheter under hela din uppväxt. Även om dina mönster har blivit relativt beständiga i vuxen ålder är de fortfarande möjliga att förändra.</p>
             </div>
             <div className="headline2">
-                <h5>Olika typer av anknytning</h5>
+                <h6><b>Olika typer av anknytning</b></h6>
             </div>
-            <br></br>
+           
             <div className="headline3">   
                 <p>Otrygg ambivalent anknytning</p>
             </div>
