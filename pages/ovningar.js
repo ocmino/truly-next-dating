0c6f61136cc4005781cd2d984ade5ef60c58d5b5
HelenaOstlund <92034375+HelenaOstlund@users.noreply.github.com>
2022-05-24 19:47:09 +0200

    new page ovningar created

---
+++ pages/ovningar.js
@@ -0,0 +1,17 @@
+import Link from "next/link"
+
+export default function Ovningar(){
+    return(
+        <div className="Infopages">
+
+            <div className="headline">
+                <Link href="/readmoredropdown"><a className="backArrow">←</a></Link>
+                <h2>Övningar som utmanar</h2>
+            </div>
+            <div className="ovningar">
+                <p>■ Den lilla och den stora människan</p>
+                <p>■ Tvärtom-metoden</p>
+            </div>
+        </div>
+    )
+}
