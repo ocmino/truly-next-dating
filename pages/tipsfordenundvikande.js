50fb4ca9ea8e06e0d887209543e37c76ec6cd389
HelenaOstlund <92034375+HelenaOstlund@users.noreply.github.com>
2022-05-24 20:10:15 +0200

    content for info page tips for den undvikande

--- pages/tipsfordenundvikande.js
+++ pages/tipsfordenundvikande.js
@@ -1,5 +1,30 @@
-export default function Tipsfordenundvikande(){
+import Link from "next/link"
+
+export default function Tipsfordenundv(){
     return(
-        <div className="Infopages"></div>
+        <div className="Infopages">
+
+            <div className="headline">
+                <Link href="/readmoredropdown"><a className="backArrow">←</a></Link>
+                <h2>6 Tips för den undvikande</h2>
+            </div>
+            <div className="tip">
+                <p><b> ● Identifiera hur du brukar tappa intresset </b>
+                <br></br>○ Agera inte på dina impulser när du får en magkänsla att det är något som är fel på din partner. Stanna upp och tänk på om detta egentligen är någon av dina strategier för att inte komma för nära personen. Är de där små defekterna du börjar uppleva kanske bara ett resultat av din relationsmodell som försöker skydda dig? Försök att hålla ut lite längre trots att det känns jobbigt!</p>
+                <p><b>● Minska fokus på ditt oberoende och fokusera mer på ömsesidigt stöd</b>
+                <br></br>○ När din partner känner att hen har en trygg bas att stötta sig mot, och inte behöver kämpa hårt för att komma nära, så kommer hen troligen att upplevas som mindre klängig. Om hen inte behöver kämpa för närheten så aktiveras inte hens relationsmodell lika mycket.</p>
+                <p><b>● Tolka din partners handlingar som om alla hade goda intentioner</b>
+                <br></br> ○ Den undvikande relationstypen gör att man tenderar att ha negativa tankar kring sin partners beteenden och intentioner. Det kan skapa en dålig stämning i relationen. 
+                <br></br>○ Du har valt att vara i en relation/dejta denna person. Försök att tolka varje handling som om den hade goda intentioner, trots att du själv kanske inte upplever det.</p>
+                <p><b>● Öva på tacksamhet i din relation</b>
+                <br></br>○ Påminn dig varje dag om att din relationstyp kan få dig att tänka negativa tankar om din partner. 
+                <br></br>○ Gör det till en vana att varje morgon eller kväll skriva ner minst en sak som du är tacksam över med din partner. Hur har hen på något (litet) sätt bidragit till din dag? 
+                <br></br>○ Målsättningen är att uppmärksamma de positiva egenskaperna hos din partner.</p>
+                <p><b>● Vänta inte på &ldquo;den enda rätta&ldquo; </b>
+                <br></br>○ Författarna till boken Attached betonar vikten av att inte vänta på den enda rätta personen. De föreslår att du ska ta en aktiv roll i processen genom att låta din partner komma dig närmare än du brukar göra. Din relationsmodell kan vara ivägen för dig och göra att det blir oerhört svårt att träffa den perfekta partnern.</p>
+                <p><b>● Använd distraktionsstrategin</b>
+                <br></br> ○ Som undvikande relationstyp kan det vara enklare att komma närmare din partner genom att göra något som distraherar. T ex gå på en promenad, paddla kajak eller göra något annat roligt tillsammans. Använd detta trick för att göra det enklare att komma närmare din partner.</p>
+            </div>
+            </div>    
     )
 }
